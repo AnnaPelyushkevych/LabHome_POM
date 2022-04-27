64fe880b94f527de763de46fb8b649ed5cbff56c
AnnaPelyushkevych <tolooklike12@gmail.com>
2022-04-27 23:00:50 +0300

    added logging
    added log4net logging functionality

--- Lab_AnnaP_home2/Lab_AnnaP_home2/Tests/RozetkaTests.cs
+++ Lab_AnnaP_home2/Lab_AnnaP_home2/Tests/RozetkaTests.cs
@@ -3,12 +3,15 @@ using Lab_AnnaP_home2.Pages;
 using log4net;
 using log4net.Config;
 using log4net.Core;
+using log4net.Repository;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Reflection;
 using System.Threading;
 using WebDriverManager.DriverConfigs.Impl;
+using System.IO;
 
 namespace Lab_AnnaP_home2
 {
@@ -16,11 +19,19 @@ namespace Lab_AnnaP_home2
     {
         private IWebDriver _driver;
         private RozetkaFilters _filters;
-        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog log = LogManager.GetLogger(typeof(Tests));
+        private static readonly ILoggerRepository repository = log4net.LogManager.GetRepository(Assembly.GetCallingAssembly());
+        //private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         [SetUp]
         public void Setup()
         {
+            // Valid XML file with Log4Net Configurations
+            var fileInfo = new FileInfo(@"Log4net.config");
+
+            // Configure default logging repository with Log4Net configurations
+            XmlConfigurator.Configure(repository, fileInfo);
+
             _filters = RozetkaFiltersJsonReader.GetFiltersObjectFromJson();
             BasicConfigurator.Configure();
 
@@ -28,6 +39,9 @@ namespace Lab_AnnaP_home2
             _driver = new ChromeDriver();
             _driver.Manage().Window.Maximize();
             _driver.Navigate().GoToUrl("https://rozetka.com.ua/");
+
+            // Log info
+            log.Info("Setup Configured");
         }
 
         [TearDown]
@@ -42,11 +56,14 @@ namespace Lab_AnnaP_home2
             // HomePage manipulations
             var homePage = new HomePage(_driver);
             homePage.SearchByKeyWord(_filters.LaptopSearch);
-
+            log.Info("Search on home page executed");
+            
             // FilterPage manipulations
             var filterPage = new FilterPage(_driver);
             filterPage.SearchByKeyWord(_filters.LaptopBrand);
             filterPage.SelectedHPBrand();
+            log.Info("Search by [HP] brand executed");
+
             filterPage.FilterByPrice();
             filterPage.SortExpensive();
             filterPage.WaitPageLoad();
@@ -57,7 +74,7 @@ namespace Lab_AnnaP_home2
             var productPage = new ProductPage(_driver);
             productPage.ImplicitWaitForSeconds(3);
             productPage.MoveToItemsListMenu();
-            productPage.ImplicitWaitForSeconds(3);
+            productPage.ImplicitWaitForSeconds(4);
             productPage.ClickAddToCart();
 
             // ShoppingCartPage manipulations
